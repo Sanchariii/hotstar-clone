898597c4a2e73d2881b5a1beea2bd65acc6e4b47
Sanchari Ray <88083502+Sanchariii@users.noreply.github.com>
2022-10-07 23:44:36 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience.',
+        image: 'images/slider 2.png'
+    },
+    {
+        name: 'loki',
+        des:'Loki, the God of Mischief, steps out of his brothers shadow to embark on an adventure that takes place after the events of "Avengers: Endgame."',
+        image: 'images/slider 1.png'
+    },
+    
+    {
+        name: 'Wanda vision',
+        des: 'Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.',
+        image: 'images/slider 3.png'
+    },
+    {
+        name: 'Raya and The Last Dragon',
+        des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.',
+        image: 'images/slider 4.png'
+    },
+    {
+        name: 'Luca',
+        des: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. Luca shares these adventures with his newfound best friend, but all the fun is threatened by a deeply held secret: he is a sea monster from another world just below the waters surface.',
+        image: 'images/slider 5.png'
+    },
+
+]
